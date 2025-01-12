2ffea9699d06d9c363f58d0ca6b4543869943eb3
hariniss12345 <hariniss12345@gmail.com>
2025-01-12 10:25:38 +0530

    feat: create User model with schema

---
+++ backend_sphere_of_art/app/models/user-model.js
@@ -0,0 +1,28 @@
+// Import the Schema and model functions from Mongoose
+import { Schema, model } from 'mongoose';
+
+/*
+ * Define the schema for the User collection in the database.
+ 
+ * The schema outlines the structure and rules for User documents.
+ */
+
+const userSchema = new Schema(
+    {
+        username: String,
+        email: String,
+        password: String,
+        role: {
+            type: String,
+            enum: ['admin', 'artist', 'customer'],
+        },
+    },
+    { timestamps: true }
+)
+
+// Create a Mongoose model for the User schema
+// This will allow interaction with the 'User' collection in the database
+const User = model('User', userSchema);
+
+// Export the User model for use in other parts of the application
+export default User;
