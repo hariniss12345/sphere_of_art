9c7621e724cf487fb1b55d4698e19f538a051c91
hariniss12345 <hariniss12345@gmail.com>
2025-01-22 08:02:01 +0530

    feat: add routes for Home, Register and Login with placeholders

--- frontend_sphere_of_art/src/App.js
+++ frontend_sphere_of_art/src/App.js
@@ -1,11 +1,28 @@
 import './App.css'
+import {Link,Routes,Route} from 'react-router-dom'
+
 
 import MainPage from './pages/MainPage'
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
 export default function App () {
     return (
         <div className = "App">
-           <MainPage/>
+          <ul id = "top-nav">
+          <li><Link to = "/home" >Home</Link></li> 
+          <li><Link to = "/register" >Register</Link></li>
+          <li><Link to = "/login" >Login</Link></li>
+          </ul>
+          
+          <Routes>
+            <Route path = "/main" element = {<MainPage/>} />
+            <Route path = "/home" element = {<Home/>} />
+            <Route path = "/register" element = {<Register/>} />
+            <Route path = "/login" element = {<Login/>} />
+
+          </Routes>
         </div>
     )
 }
