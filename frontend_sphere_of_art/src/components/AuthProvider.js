6eeb959991a51e84c2841d1ba0ba40a5d0223bba
hariniss12345 <hariniss12345@gmail.com>
2025-01-22 08:49:50 +0530

    feat: add reducer function to handle LOGIN and LOGOUT actions

--- frontend_sphere_of_art/src/components/AuthProvider.js
+++ frontend_sphere_of_art/src/components/AuthProvider.js
@@ -1,5 +1,7 @@
 import AuthContext from '../context/Auth.js'
 import {useReducer,useEffect} from 'react'
+import userReducer from '../reducers/userReducer.js'
+import axios from 'axios'
 
 const initialState={
     isLoggedIn:false,
@@ -9,10 +11,31 @@ const initialState={
 export default function AuthProvider(props){
     const[ userState,userDispatch]=useReducer(userReducer,initialState)
 
+    const handleLogin=(user)=>{
+        return userDispatch({type:'LOGIN',payload:{isLoggedIn:true,user:user}})
+    }
+    
+    const handelLogout=()=>{
+        return userDispatch({type:'LOGOUT',payload:{isLoggedIn:false,user:null}})
+    }
+
+    useEffect(()=>{
+        (async ()=>{
+            if(localStorage.getItem('token')){
+                const response=await axios.get('http://localhost:4700/api/users/profile',{headers:{Authorization:localStorage.getItem('token')}})
+                handleLogin(response.data)
+            }
+        })();
+    },[])     
+
+    //to handle page loads
+    if(localStorage.getItem('token')&& !userState.user){
+        return <p>loading...</p>
+    }
 
     return (
         <div>
-            <AuthContext.Provider >
+            <AuthContext.Provider value={{userState,handleLogin,handelLogout}}>
                 {props.children}
             </AuthContext.Provider>     
         </div>
