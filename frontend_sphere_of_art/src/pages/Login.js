e22a9ffc9993027e01187e4d198a23124f6b2e35
hariniss12345 <hariniss12345@gmail.com>
2025-01-23 07:40:45 +0530

    feat: design login form with necessary fields and implement dashboard page

--- frontend_sphere_of_art/src/pages/Login.js
+++ frontend_sphere_of_art/src/pages/Login.js
@@ -1,7 +1,47 @@
+import { useState } from "react"
+
 export default function Login(){
-    return (
-        <div>
-            login page
+  
+    const [formData, setFormadata] = useState({
+        username:"",
+        email : "",
+        password : ""
+    });
+
+
+    return(
+        <div className="login">
+            <h2>Login page</h2>
+           
+            <form>
+            <input 
+                    type="text" 
+                    value={formData.username} 
+                    onChange={(e)=> setFormadata({...formData, username:e.target.value})}
+                    placeholder="Enter username" 
+                />
+                
+                <br/>
+                <input 
+                    type="email" 
+                    value={formData.email} 
+                    onChange={(e)=> setFormadata({...formData, email:e.target.value})}
+                    placeholder="Enter email" 
+                />
+              
+                <br/>
+
+                <input 
+                    type="password" 
+                    value={formData.password} 
+                    onChange={(e)=> setFormadata({...formData, password:e.target.value})}
+                    placeholder="Enter password"
+                />               
+                <br/>
+              
+                <input type="submit"/>
+            </form>
         </div>
     )
 }
+
