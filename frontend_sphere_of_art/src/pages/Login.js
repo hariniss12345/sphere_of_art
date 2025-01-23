df72cb6d6b4b654b2aa5c13efad7d8cc1556592f
hariniss12345 <hariniss12345@gmail.com>
2025-01-23 07:45:52 +0530

    feat: implement client-side validation and form submission

--- frontend_sphere_of_art/src/pages/Login.js
+++ frontend_sphere_of_art/src/pages/Login.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-
+import axios from 'axios'; 
 export default function Login(){
   
     const [formData, setFormadata] = useState({
@@ -7,20 +7,59 @@ export default function Login(){
         email : "",
         password : ""
     });
+    const [clientErrors, setClientErros] = useState(null);
+   
+    const clientValidationsErrors = {};
 
+
+    const runClientValidations = ()=>{
+        if(formData.username.trim().length === 0){
+            clientValidationsErrors.username = 'username is required'
+        }
+
+        if(formData.password.trim().length === 0){
+            clientValidationsErrors.password = 'password is required'
+        }
+
+        if(formData.email.trim().length === 0){
+            clientValidationsErrors.email = 'email is required'
+        }
+
+    }
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        runClientValidations();
+        if(Object.keys(clientValidationsErrors).length == 0) {
+            try {
+               const response = await axios.post('http://localhost:4700/api/users/login',formData)
+               console.log(response.data)
+               localStorage.setItem('token',response.data.token)
+               const userResponse=await axios.get('http://localhost:4700/api/users/account',{headers:{Authorization:localStorage.getItem('token')}})
+               console.log(userResponse.data)
+               
+            } catch(err) {
+                console.log(err.message)
+            }
+            setClientErros({})
+        } else {
+            
+            setClientErros(clientValidationsErrors); 
+        }
+    }
 
     return(
         <div className="login">
             <h2>Login page</h2>
-           
-            <form>
+            
+            <form onSubmit={handleSubmit}>
             <input 
                     type="text" 
                     value={formData.username} 
                     onChange={(e)=> setFormadata({...formData, username:e.target.value})}
                     placeholder="Enter username" 
                 />
-                
+                {clientErrors && <span style={{color:'red'}}>{clientErrors.username}</span>}
                 <br/>
                 <input 
                     type="email" 
@@ -28,7 +67,7 @@ export default function Login(){
                     onChange={(e)=> setFormadata({...formData, email:e.target.value})}
                     placeholder="Enter email" 
                 />
-              
+               {clientErrors && <span style={{color:'red'}}>{clientErrors.email}</span>}
                 <br/>
 
                 <input 
@@ -36,7 +75,8 @@ export default function Login(){
                     value={formData.password} 
                     onChange={(e)=> setFormadata({...formData, password:e.target.value})}
                     placeholder="Enter password"
-                />               
+                /> 
+                {clientErrors && <span style={{color:'red'}}>{clientErrors.password}</span>}
                 <br/>
               
                 <input type="submit"/>
