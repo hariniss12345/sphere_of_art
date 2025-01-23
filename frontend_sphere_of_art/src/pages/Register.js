bb514a5b6f1e5608d7ed960c88cffdbd9403b02d
hariniss12345 <hariniss12345@gmail.com>
2025-01-23 06:57:28 +0530

    feat: design registration form with necessary fields and validations

--- frontend_sphere_of_art/src/pages/Register.js
+++ frontend_sphere_of_art/src/pages/Register.js
@@ -1,7 +1,61 @@
+import '../App.css';
+import { useState } from "react"
+
 export default function Register(){
-    return (
-        <div>
-            register page
-        </div>
+    
+    const [formData, setFormdata] = useState({
+        username:"",
+        email : "",
+        password : "",
+        role : ""
+    });
+
+
+    return(
+        <>
+            <h2>Register page</h2>
+            <form >
+            <input 
+                    type="text" 
+                    value={formData.username} 
+                    onChange={(e)=> setFormdata({...formData, username:e.target.value})}
+                    placeholder="Enter username" 
+                /><br/>
+            
+                <input 
+                    type="email" 
+                    value={formData.email} 
+                    onChange={(e)=> setFormdata({...formData, email:e.target.value})}
+                    placeholder="Enter email" 
+                /><br/>
+                
+                <input 
+                    type="password" 
+                    value={formData.password} 
+                    onChange={(e)=> setFormdata({...formData, password:e.target.value})}
+                    placeholder="Enter password"
+                /> <br />
+                
+                <input 
+                    type="radio" 
+                    name="role"  
+                    value="customer"
+                    id="customer" 
+                    onChange={(e)=> setFormdata({...formData, role:e.target.value})} 
+                /><label htmlFor="customer" name="customer">Customer</label>
+
+                <input 
+                    type="radio" 
+                    name="role" 
+                    value="artist" 
+                    id="artist"
+                    onChange={(e)=> setFormdata({...formData, role:e.target.value})} 
+                />
+                <label htmlFor="artist" name="artist">Artist</label><br />
+               
+                <input type="submit" value="sign up"/>
+            </form>
+        </>
     )
 }
+
