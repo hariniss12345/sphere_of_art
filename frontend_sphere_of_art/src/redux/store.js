51fa504c5e1c83ccc17166b1dbb4390654ef7582
hariniss12345 <hariniss12345@gmail.com>
2025-01-31 21:47:21 +0530

    feat: created order slice,order form,order page

--- frontend_sphere_of_art/src/redux/store.js
+++ frontend_sphere_of_art/src/redux/store.js
@@ -4,11 +4,13 @@ import { configureStore } from "@reduxjs/toolkit";
 // Importing the artist slice reducer to manage the artists' state in the store
 import artistReducer from "./slices/artistSlice";
 
+
+
 // Configuring the Redux store with the artist slice
 const store = configureStore({
     reducer: {
         artists: artistReducer, // Adding the artistReducer to handle artists-related state
-        // Add more slices here later if needed
+        
     },
 });
 
