0545b03952017cba3172971011750a97cec740ba
hariniss12345 <hariniss12345@gmail.com>
2025-02-23 19:54:22 +0530

    refactor: modified index file in frontend

--- frontend_sphere_of_art/src/index.js
+++ frontend_sphere_of_art/src/index.js
@@ -1,42 +1,27 @@
-// Importing React library for building user interfaces
 import React from 'react';
-
-// Importing ReactDOM for rendering the React application into the DOM
 import ReactDOM from 'react-dom/client';
-
-
 import './index.css';
-
-// Importing the main application component
 import App from './App';
-
-// Importing BrowserRouter from react-router-dom for enabling routing in the app
 import { BrowserRouter } from 'react-router-dom';
-
-// Importing Provider from react-redux to integrate Redux state management
 import { Provider } from 'react-redux';
-
-// Importing the Redux store to provide global state to the application
-import store from './redux/store';      
-
-// Importing the custom AuthProvider component to handle authentication context
+import store from './redux/store';
 import AuthProvider from './components/AuthProvider.js';
+import { SocketProvider } from './context/SocketContext';  // import the provider
 
-console.log('store',store.getState())
+console.log('store', store.getState());
 
-store.subscribe(()=>{
-  console.log('state get updated',store.getState())
-})
+store.subscribe(() => {
+  console.log('state updated', store.getState());
+});
 
-// Creating a root element for React rendering
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// Rendering the application inside the root element
 root.render(
-  <Provider store={store}>  {/* Wrapping the app with Redux Provider to enable global state management */}
-    <BrowserRouter>  {/* Wrapping with BrowserRouter to enable routing */}
-      <AuthProvider>  {/* Wrapping with AuthProvider to manage authentication state */}
-        <App />  {/* Rendering the main application component */}
+  <Provider store={store}>
+    <BrowserRouter>
+      <AuthProvider>
+        <SocketProvider>
+          <App />
+        </SocketProvider>
       </AuthProvider>
     </BrowserRouter>
   </Provider>
